3b49ddc5d78d182a4884051056b30adf4cbca2c8
manideep <17101992m@gmail.com>
2023-01-30 15:09:44 +0530

    Nested routing added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Outlet } from 'react-router-dom'
 import Home from "./routes/home/home.component";
+
+const Navigation = () => {
+  return (
+    <div>
+      <div>
+        <h1>I am navigation bar</h1>
+        <Outlet />
+      </div>
+    </div>
+  )
+}
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/home' element={<Home />} />
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+      </Route>
     </Routes>
   )
 }
