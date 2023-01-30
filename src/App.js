d80cfd279515e8c60b119689cc7702eba675a250
Manideep <17101992m@gmail.com>
2023-01-30 22:14:30 +0530

    singin route added and the link to the singin page added to the page and firebase installed to the project

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import { Routes, Route, Outlet } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
+import Navigation from './routes/navigation/navigation.component';
 import Home from "./routes/home/home.component";
+import SignIn from './routes/sign-in/sign-in.component';
 
-const Navigation = () => {
-  return (
-    <div>
-      <div>
-        <h1>I am navigation bar</h1>
-        <Outlet />
-      </div>
-    </div>
-  )
-}
 
 const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
+        <Route path='sign-in' element={<SignIn />} />
       </Route>
     </Routes>
   )
