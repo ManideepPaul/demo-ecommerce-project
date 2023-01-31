85107a25543884b24a38f715812dcda4e00c1e0e
Manideep <17101992m@gmail.com>
2023-01-31 21:49:12 +0530

    signInWithGoogleRedirect added to the project

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,17 +1,41 @@
-import { signInWithGooglePopup, createUserDoucumentFromAuth } from "../../utils/firebase/firebase.utils";
+import { useEffect } from "react";
+import { getRedirectResult } from "firebase/auth";
+
+import {
+  auth,
+  signInWithGooglePopup,
+  createUserDoucumentFromAuth,
+  signInWithGoogleRedirect,
+} from "../../utils/firebase/firebase.utils";
 
 const SignIn = () => {
-    const logGoogleUser = async () => {
-        const { user } = await signInWithGooglePopup();
-        const userDocRef = await createUserDoucumentFromAuth(user)
-        // console.log(user)
+    // the useEffect will get the user authentication result after redirected from google
+  useEffect(() => redirectResult, []);
+
+  const redirectResult = async () => {
+    const response = await getRedirectResult(auth); // this helps us to get validate user if sign is was failed or passed after the redirect to google.
+
+    if (response) {
+      await createUserDoucumentFromAuth(response.user);
+      console.log(response.user);
     }
-    return (
-        <>
-            <h1>SignIn Page</h1>
-            <button onClick={logGoogleUser}>SIGNIN with Google</button>
-        </>
-    )
-}
+  };
+
+  const logGoogleUser = async () => {
+    const { user } = await signInWithGooglePopup();
+    const userDocRef = await createUserDoucumentFromAuth(user);
+    // console.log(user)
+  };
+
+  return (
+    <>
+      <h1>SignIn Page</h1>
+      <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+      <button onClick={signInWithGoogleRedirect}>
+        sign in with Google Redirect
+      </button>
+    </>
+  );
+};
 
 export default SignIn;
