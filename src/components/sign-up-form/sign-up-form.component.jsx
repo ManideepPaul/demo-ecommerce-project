3a7a4fc52ef5909b98a09c89d5803a61e378bb8b
Manideep <17101992m@gmail.com>
2023-02-02 13:46:09 +0530

    create sign up component, the form input component, button component

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,58 +1,90 @@
 import { useState } from "react";
-import { createAuthWithEmailAndPassword, createUserDoucumentFromAuth } from "../../utils/firebase/firebase.utils";
+import {
+  createAuthWithEmailAndPassword,
+  createUserDoucumentFromAuth,
+} from "../../utils/firebase/firebase.utils";
+
+import FormInput from "../form-input/form-input.component";
+import Button from "../button/button.component";
+import './sign-up-form.styles.scss'
 
 const defaultFormFields = {
-    'displayName': '',
-    'email': '',
-    'password': '',
-    'confirmPassword': ''
-}
+  displayName: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
+};
 
 const SignUpForm = () => {
+  const [formFields, setFormFields] = useState(defaultFormFields);
+  const { displayName, email, password, confirmPassword } = formFields;
+  // console.log(formFields)
 
-    const [formFields, setFormFields] = useState(defaultFormFields)
-    const { displayName, email, password, confirmPassword } = formFields;
-    // console.log(formFields)
+  const handleSubmit = async (event) => {
+    event.preventDefault();
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
+    if (password !== confirmPassword) alert("Password mismatch");
 
-        if (password !== confirmPassword) alert('Password mismatch')
-
-        try {
-            let { user } = await createAuthWithEmailAndPassword(email, password);
-            // console.log(response)
-            user = {...user, displayName} // because in this case user object will contain displayName as null assigining the displayName value into the user object.
-            await createUserDoucumentFromAuth(user)
-        } catch (error) {
-            if(error.code === 'auth/email-already-in-use') alert('Email already exist');
-            else console.log(error)
-        }
+    try {
+      let { user } = await createAuthWithEmailAndPassword(email, password);
+      // console.log(response)
+      user = { ...user, displayName }; // because in this case user object will contain displayName as null assigining the displayName value into the user object.
+      await createUserDoucumentFromAuth(user);
+    } catch (error) {
+      if (error.code === "auth/email-already-in-use")
+        alert("Email already exist");
+      else console.log(error);
     }
+  };
 
-    const handleChage = (event) => {
-        const { name, value } = event.target;
-        setFormFields({ ...formFields, [name]: value })
-    }
-    return (
-        <div>
-            <h1>Sign up with your email and password</h1>
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="">Display Name</label>
-                <input type="text" required value={displayName} onChange={handleChage} name='displayName' />
+  const handleChage = (event) => {
+    const { name, value } = event.target;
+    setFormFields({ ...formFields, [name]: value });
+  };
+  return (
+    <div className="sign-up-container">
+        <h2>Don't have an account</h2>
+      <span>Sign up with your email and password</span>
+      <form onSubmit={handleSubmit}>
+        <FormInput
+          label="Display Name"
+          type="text"
+          required
+          value={displayName}
+          onChange={handleChage}
+          name="displayName"
+        />
 
-                <label htmlFor="">Email</label>
-                <input type="email" required value={email} onChange={handleChage} name='email' />
+        <FormInput
+          label="Email"
+          type="email"
+          required
+          value={email}
+          onChange={handleChage}
+          name="email"
+        />
 
-                <label htmlFor="">Password</label>
-                <input type="password" required value={password} onChange={handleChage} name='password' />
+        <FormInput
+          label="Password"
+          type="password"
+          required
+          value={password}
+          onChange={handleChage}
+          name="password"
+        />
 
-                <label htmlFor="">Confirm Password</label>
-                <input type="password" required value={confirmPassword} onChange={handleChage} name='confirmPassword' />
-                <button type="submit">Sign Up</button>
-            </form>
-        </div>
-    )
-}
+        <FormInput
+          label="Confirm Password"
+          type="password"
+          required
+          value={confirmPassword}
+          onChange={handleChage}
+          name="confirmPassword"
+        />
+        <Button buttonType='inverted' type="submit">Sign Up</Button>
+      </form>
+    </div>
+  );
+};
 
-export default SignUpForm
+export default SignUpForm;
